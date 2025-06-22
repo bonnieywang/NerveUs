a125b91fbdc0342e2ed9dee500795e07da88f044
bonnieywang <149812277+bonnieywang@users.noreply.github.com>
2025-06-22 13:21:38 -0400

    added node.js, added nerve injury lottie file animations, added callout tutorial boxes

---
+++ scripts/lottieanimation.js
@@ -0,0 +1,11 @@
+LottieInteractivity.create({
+  player: "#compressioncrush-animation",
+  mode: "scroll",
+  actions: [
+    {
+      visibility: [0, 1],
+      type: "seek",
+      frames: [0, 80],
+    },
+  ],
+});
